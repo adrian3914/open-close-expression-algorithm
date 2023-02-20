38a5bafdeb87ed0da544658f810ea21ccbbc291b
adrian.pazalonso <adrian.pazalonso@cgi.com>
2023-02-20 12:53:50 -0500

    fix bug same quantity of closing and opening char

--- src/app/open-close-expression-checker/open-close-expression-checker.component.ts
+++ src/app/open-close-expression-checker/open-close-expression-checker.component.ts
@@ -10,76 +10,97 @@ export class OpenCloseExpressionCheckerComponent {
   openChars = ['(', '{', '<', '['];
   closeChars = [')', '}', '>', ']'];
   isExpressionValid: boolean = false;
-  isVerified: boolean = true;
+  isVerified: boolean = false;
 
   verifyExpression() {
     if (this.expression.length === 0) {
       return;
     }
+
     this.isVerified = true;
     let openCharsStack: string[] = [];
     let chars = [...this.expression];
     let status = true;
     let closeCharCounter = 0;
 
-    chars.forEach((char) => {
+    if (this.verifyExpressionContainsChars(chars)) {
+      chars.forEach((char) => {
 
-      if (this.openChars.includes(char)) {
-        openCharsStack.push(char);
-      }
+        if (this.openChars.includes(char)) {
+          openCharsStack.push(char);
+        }
 
-      if (this.closeChars.includes(char)) {
-        if (openCharsStack.length === 0) {
+        if (this.closeChars.includes(char)) {
+          if (openCharsStack.length === 0) {
+            this.isExpressionValid = false;
+            return;
+          }
+
+          closeCharCounter++;
+
+          switch (char) {
+            case ')' : {
+              if (openCharsStack.pop() !== '(') {
+                status = false;
+              }
+              break;
+            }
+
+            case '}': {
+              if (openCharsStack.pop() !== '{') {
+                status = false;
+              }
+              break;
+            }
+
+            case '>': {
+              if (openCharsStack.pop() !== '<') {
+                status = false;
+              }
+              break;
+            }
+
+            case ']' : {
+              if (openCharsStack.pop() !== '[') {
+                status = false;
+              }
+              break;
+            }
+
+          }
+        }
+
+        if (!status) {
           this.isExpressionValid = false;
           return;
         }
+      });
 
-        closeCharCounter++;
+      this.expression = '';
 
-        switch (char) {
-          case ')' : {
-            if (openCharsStack.pop() !== '(') {
-              status = false;
-            }
-            break;
-          }
-
-          case '}': {
-            if (openCharsStack.pop() !== '{') {
-              status = false;
-            }
-            break;
-          }
-
-          case '>': {
-            if (openCharsStack.pop() !== '<') {
-              status = false;
-            }
-            break;
-          }
-
-          case ']' : {
-            if (openCharsStack.pop() !== '[') {
-              status = false;
-            }
-            break;
-          }
-
-        }
-      }
-
-      if (!status) {
-        this.isExpressionValid = false;
-        return;
-      }
-    });
-
-    this.expression = '';
-
-    this.isExpressionValid = status && closeCharCounter > 0;
+      this.isExpressionValid = ( status && closeCharCounter > 0 && openCharsStack.length === 0);
+    } else {
+      this.expression = '';
+      this.isExpressionValid = true;
+    }
   }
 
   setIsVerified() {
     this.isVerified = false;
+  }
+
+  private verifyExpressionContainsChars(expressionChars: any[]) {
+    let allOpenANdCLoseChars = [...this.openChars, ...this.closeChars];
+    let status = false;
+
+    expressionChars.forEach(
+      c => {
+        if (allOpenANdCLoseChars.includes(c)) {
+          status = true;
+        }
+      }
+    );
+
+    return status;
   }
 }
